7f8363950e3740817ca140d739466aede298ce3e
zhaoyong <1733360217@qq.com>
2023-03-03 12:11:12 +0800

    text03

--- bk-hd/src/main/java/com/zy/bk/BKApplication.java
+++ bk-hd/src/main/java/com/zy/bk/BKApplication.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class BKApplication {
     public static void main(String[] args) {
         SpringApplication.run(BKApplication.class,args);
-        log.info("项目启动成功-----------------------------------------------");
+        log.info("项目启动成功------------------------------------------------");
 
 
     }
