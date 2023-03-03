0692a4c6c3078e0c0e0eea38973d86a3a0fb37c8
zhaoyong <1733360217@qq.com>
2023-03-03 10:36:24 +0800

    第一次提交

---
+++ bk-hd/src/main/java/com/zy/bk/config/MybatisPlusConfig.java
@@ -0,0 +1,18 @@
+package com.zy.bk.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@MapperScan("com.zy.bk.mapper")
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
